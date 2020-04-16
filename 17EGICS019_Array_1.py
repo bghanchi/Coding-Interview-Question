54fb66e541559458223607ac625e8f616ab861b5
Chinmay Jain <chinmayrjain1@gmail.com>
2020-04-17 01:44:46 +0530

    Add task1 and task2 of Array

---
+++ 17EGICS019_Array_1.py
@@ -0,0 +1,3 @@
+arr = [7, 5, 5, 6, 9, 4, 5, 4, 4, 5, 9, 5, 4] 
+res = max(set(arr), key = arr.count) 
+print(res)
---
+++ 17EGICS019_Array_2.py
@@ -0,0 +1,2 @@
+arr = [7, 1, 5, 6, 1, 4, 5, 4, 4, 5, 9, 5, 4, 1] 
+print(min(sum(arr[:(len(arr)//2)]), sum(arr[(len(arr)//2):])))
