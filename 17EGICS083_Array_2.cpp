6a6a2805daed9a98c21813895e28b22dd5a510e7
Purvi Dwivedi <purvidwi@gmail.com>
2020-04-17 14:37:44 +0530

    17EGICS083_ARRAY

--- 17EGICS083_Array_2.cpp
+++ 17EGICS083_Array_2.cpp
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,j,sum,sum1,a[n],b[n];
+	int n,i,j,sum=0,sum1=0;
 	printf("Enter size of the array(in even number)");
 	scanf("%d",&n);
+	int a[n];
 	printf("Enter array elements:");
 	for(i=0;i<n;i++)
 	{
@@ -11,18 +12,19 @@ int main()
 	}
 	for(i=0;i<(n/2);i++)
 	{
-		sum=sum + a[i];
+		sum= sum + a[i];
 	}
 	for(j=(n/2);j<n;j++)
 	{
-		sum1=sum1 + b[i];
+		sum1= sum1 + a[j];
 	}
-	if(sum<sum1)
-	{
-		printf("%d",sum);
-	}
-	else
+	if(sum>sum1)
 	{
 		printf("%d",sum1);
 	}
+	else
+	{
+		printf("%d",sum);
+	}
+	
 }
