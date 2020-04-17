80617bc2ccb5aa6051bf6aafc3c72b7fa96a5481
agarwalgeetika2000 <39965675+agarwalgeetika2000@users.noreply.github.com>
2020-04-17 17:43:49 +0530

    Assignment 1

--- 17egics031_array_2.py
+++ 17egics031_array_2.py
@@ -1,23 +1,21 @@
-list=[]
-n=int(input("Enter the size of array: "))
-mid=int(n/2)
-sum1=0
-sum2=mid+1
-if (n<=1000000 and n%2==0):
-    for i in range(0,n):
-        item=int(input())
-        list.append(item)
-    print(list)
-    for i in range(0,mid):
-        sum1+=list[i]
-    print("Sum of the list 1: ",sum1)
-    for i in range(mid+1,n):
-        sum2+=list[i]
-    print("Sum of list 2: ",sum2)
-    if sum1<sum2:
-        print("Minimum Sum is: ",sum1)
-    else:
-        print("Minimum Sum is :",sum2)
+def minSum(arr,s):
+    s=int(s/2)
+    first=arr[:s]
+    second=arr[s:]
+    sum1=sum(first)
+    sum2=sum(second)
+    return min(sum1,sum2)
+
+size=int(input(("Enter the size of array: ")))
+s=size
+arr=[]
+if (s<=1000000 and s%2==0):
+    for size in range(s):
+        num=int(input())
+        arr.append(num)
+    
+print("MINIMUM SUM: ",minSum(arr,s))
+
         
 
 
