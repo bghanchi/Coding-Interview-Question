32d8e6ab572a1bd6dc1414b3eb9adae5f51651e1
Gauri Shrimali <47269380+GauriShrimali@users.noreply.github.com>
2020-04-18 02:33:33 +0530

    The problem can be solved by above two methods.

--- 17EGICS030_ARRAY_4.py
+++ 17EGICS030_ARRAY_4.py
@@ -1,10 +1,21 @@
 #Given a character array of size N. You have to perform the right shift by s places.
+
+#SOLUTION 1
+#  def rightShift(arr,rt):
+#     while rt != 0:
+#         p=arr.pop()
+#         arr.insert(0,p)
+#         rt=rt-1
+#     return arr
+
+#SOLUTION 2
 def rightShift(arr,rt):
-    while rt != 0:
-        p=arr.pop()
-        arr.insert(0,p)
-        rt=rt-1
-    return arr
+    while rt>len(arr):
+        rt-=len(arr)
+    s1=arr[rt:]
+    s2=arr[0:rt]
+    s=s1+s2
+    return s
 
 size=int(input(("Enter the size of array: ")))
 s=size
