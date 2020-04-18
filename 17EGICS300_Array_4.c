d016f7816e5adfb172b53ba31f6d854840cba56b
Vishal7296 <47269395+Vishal7296@users.noreply.github.com>
2020-04-18 11:48:31 +0530

    Update 17EGICS300_Array_4.c

--- 17EGICS300_Array_4.c
+++ 17EGICS300_Array_4.c
@@ -2,18 +2,14 @@
 #include<stdlib.h>
 int main()
 {
-char *arr;
+
 int n,s,i,j,temp;
 printf("Enter the size of array\n");
 scanf("%d",&n);
-n=n+1;
-arr=(char*)malloc(n*sizeof(char));
-for(i=0;i<n;i++)
-scanf("%c",&arr[i]);
+char arr[n];
+scanf("%s",&arr);
 printf("Enterno of place to shift\n");
 scanf("%d",&s);
-if(s>n)
-s=s+1;
 for(i=0;i<s;i++)
 {
 	temp=arr[n-1];
@@ -21,11 +17,10 @@ for(i=0;i<s;i++)
 	{
 		arr[j]=arr[j-1];
 	}
-	arr[j]=temp;
+	arr[0]=temp;
 }
 printf("After shifting character array is \n");
-for(i=0;i<n;i++)
-printf("%c",arr[i]);
+printf("%s",arr);
 
 return 0 ;
 }
