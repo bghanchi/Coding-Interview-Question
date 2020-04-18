172087c370d2fb51e62a91733b076dbc4f85dd13
Riya Soni <riyasoni5990@gmail.com>
2020-04-18 23:17:02 +0530

    17EGICS092 Day2 and 3

---
+++ 17EGICS092_Array_5.py
@@ -0,0 +1,25 @@
+n=int(input())
+a=[]
+dict={}
+sum,c=0,0
+for i in range(n):
+	a.append(int(input()))
+for i in range(0,n):
+	if i<(n-1):
+		if a[i]<a[i+1]:
+			sum=sum+a[i]
+			c+=1
+		else:
+			sum=sum+a[i]
+			c+=1
+			dict[sum]=c
+			sum,c=0,0
+	else:
+		sum=sum+a[i]
+		c+=1
+		dict[sum]=c
+print(max(dict,key=dict.get))
+
+	
+		
+
