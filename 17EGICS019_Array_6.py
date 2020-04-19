bd216fa718d2f0a015c94e694930cc68684748c2
Chinmay Jain <chinmayrjain1@gmail.com>
2020-04-19 12:58:48 +0530

    Corrected Task 6 Program

--- 17EGICS019_Array_6.py
+++ 17EGICS019_Array_6.py
@@ -5,7 +5,10 @@ try:
     for i in range(n):
         arr.append(int(input()))
     res = max(arr)
-    arr = list(filter(res.__ne__, arr)) 
-    print("\nProduct : ", res * max(arr))
+    if arr.count(res) > 1:
+        print("\nProduct : ", res * res)
+    else:
+        arr = list(filter(res.__ne__, arr))
+        print("\nProduct : ", res * max(arr))
 except:
     print("Please provide valid inputs!")
