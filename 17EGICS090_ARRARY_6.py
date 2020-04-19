e30265bec08d4692728be7c013d0b905286e289d
Bharat Kumar <bghanchi1997@gmail.com>
2020-04-19 12:15:24 +0530

    Revert "17EGICS090 Day 3 Done"

--- 17EGICS090_ARRARY_6.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-array = []
-for i in range(0,n):
-	array.append(int(input()))
-maxelement = max(array)
-array.pop(array.index(maxelement))
-nextmaxelement = max(array)
-print()
-print(maxelement*nextmaxelement)
