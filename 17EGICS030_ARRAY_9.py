563c116ec2c16341fe0b2d728b3fc44f1df64465
Gauri Shrimali <47269380+GauriShrimali@users.noreply.github.com>
2020-04-23 23:57:08 +0530

    ARRAY_4 ALL SOLUTIONS(7,8,9)

---
+++ 17EGICS030_ARRAY_9.py
@@ -0,0 +1,26 @@
+#Given an array arr[] of n integers, construct a Product Array prod[] (of same size) such that 
+#prod[i] is equal to the product of all the elements of arr[] except arr[i]. Solve it without division 
+#operator and in O(n)
+
+#input
+size=int(input(("Enter the size of array: ")))
+s=size
+arr=[]
+result=[]
+
+while size != 0:
+    num=int(input())
+    arr.append(num)
+    size=size-1
+
+#logic
+copyArr=arr.copy()
+
+for i in range(len(arr)):
+    rem=arr[i]
+    copyArr.remove(rem)
+    result.append(math.prod(copyArr))
+    copyArr=arr.copy()
+
+print("RESULTANT ARRAY IS :- ")
+print(result)
