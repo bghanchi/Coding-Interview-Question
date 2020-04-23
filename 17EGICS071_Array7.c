657d87ccc3e0421861968a04e39ae5d99a76658e
nehasoni464 <47269263+nehasoni464@users.noreply.github.com>
2020-04-23 15:52:41 +0530

    updated

--- 17EGICS071_Array7.c
+++ 17EGICS071_Array7.c
@@ -1,26 +1,27 @@
 #include<stdio.h>
 #include<conio.h>
 int main()
-{
-	int n,i,j;
+{	int n,i,j,c=0;
 	printf("enter the size of an array");
 	scanf("%d",&n);
 	int a[n];
 	printf("enter the elements");
 	for(i=0;i<n;i++)
-	{ 
-	  scanf("%d",&a[i]);
-	  
-		}
-		printf("leaders are:");
-		for(i=0,j=i+1;j<n-1,i<n-1;j++,i++)
-		
-		{
-			if(a[i]<a[j]||a[j]==a[n-1])
-		     {
-		     	printf("%d  ",a[j]);
-			 }
-			}	
+	{ scanf("%d",&a[i]);}
+	printf("leaders are:");
+		for(i=0;i<n;i++){
+			int c=0;
+			for(j=i+1; j<n;j++)
+			{	if(a[i]<=a[j])
+		    	 {	c++;
+		    	 break;
+				 }
+			}
+			
+			if(c==0 || a[i]==a[n-1])
+			{
+				printf("%d  ",a[i]);
+		}	}
 
 getch();
 return 0;
