314499edc6ab76a07bdf82a1322cf8ed2303e9d5
rhythmbhiwani <rhythmbhiwani@gmail.com>
2020-04-24 23:19:27 +0530

    Task 10 and 11 done and all arrenged

---
+++ Array/17EGICS090_ARRAY_10.py
@@ -0,0 +1,11 @@
+#Task 10
+n=int(input())
+t=int(input())
+array = list(map(int, input().split()))
+for i in range(0,n):
+	if array[i]==t:
+		print(i)
+		break
+	elif array[i]>t:
+		print(i)
+		break
