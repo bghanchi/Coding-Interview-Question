e8cf94b2142ddfd175b8eaada9efa432de3493f0
nehasoni464 <47269263+nehasoni464@users.noreply.github.com>
2020-04-24 22:30:07 +0530

    Create 17EGICS071_Array10.c

---
+++ 17EGICS071_Array10.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ int n,flag=0,i,t;
+  printf("Enter size of an array");
+  scanf("%d",&n);
+  int a[n];
+  printf("Enter elements of an array");
+  for(i=0;i<n;i++)
+  {
+  	scanf("%d",&a[i]);
+  	
+  }
+	printf("enter the target value");
+	scanf("%d",&t);
+	for(i=0;i<n;i++)
+	{
+		if( a[i]==t )
+		{ flag=1;
+	      printf("%d",i);
+	      break;
+		 }
+	if(flag==0 && a[i]>t)
+	{  printf("%d",i);
+		break;
+	   
+		
+	}}
+
+	
+	
+	
+	getch();
+	return 0;
+}
